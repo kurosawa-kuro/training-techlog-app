b3544ef2950288fc417cfb0bc1e99dbeaf523876
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 11:45:42 +0000

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'posts/new'
   devise_for :users
   root 'home#top'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
