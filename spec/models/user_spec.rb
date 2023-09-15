ac1ab3ece2c2beaa6875423f4591e694b84e47f2
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 09:48:30 +0000

    add

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,12 +3,11 @@ require 'rails_helper'
 describe User do
   let(:nickname) { 'テスト太郎' }
   let(:email) { 'test@example.com' }
-  let(:password) { '12345678' }
   let(:user) { User.new(nickname: nickname, email: email, password: password, password_confirmation: password) } # 変数に格納
 
   describe '.first' do
     before do
-      create(:user, nickname: nickname, email: email)
+      @user = create(:user, nickname: nickname, email: email)
     end
 
     subject { described_class.first }
@@ -17,9 +16,11 @@ describe User do
       expect(subject.nickname).to eq('テスト太郎')
       expect(subject.email).to eq('test@example.com')
     end
+
   end
 
   describe 'validation' do
+    let(:password) { '12345678' }
 
     describe 'nickname属性' do
       describe '文字数制限の検証' do
@@ -38,7 +39,7 @@ describe User do
             user.valid?
 
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include('is too long (maximum is 20 characters)')
+            expect(user.errors[:nickname]).to include('は20文字以下に設定して下さい。')
           end
         end
       end
@@ -49,7 +50,7 @@ describe User do
 
           it 'User オブジェクトは無効である' do
             expect(user.valid?).to be(false)
-            expect(user.errors[:nickname]).to include("can't be blank")
+            expect(user.errors[:nickname]).to include('が入力されていません。')
           end
         end
       end
