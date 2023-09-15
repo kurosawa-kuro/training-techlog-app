2be8fdaea3b85e3f1d5ce199004ea77fed3880c8
Toshifumi Kurosawa <kuromailserver@gmail.com>
2023-09-15 08:30:29 +0000

    add

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+RSpec.describe 'Homes', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  describe 'トップページの検証' do
+    it 'Home#top という文字列が表示される' do
+      visit '/'
+
+      # p page.text
+      expect(page).to have_content('Home#top')
+    end
+  end
+end
